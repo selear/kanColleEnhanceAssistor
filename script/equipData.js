48927957859073bc12495e8e92520df4e865dd70
selear <selear@gmail.com>
2015-05-20 02:36:17 +0800

    增强文件内代码的可读性
    1. kanColle.remodel.defaultChecked对象更名为defaultSelected
    2. kanColle.remodel.equips对象独立设定, 每个装备的数据都独立进行编写; 一来装备的template更为明确;
    二来方便查找和修改;

--- script/equipData.js
+++ script/equipData.js
@@ -1,356 +1,7 @@
 var kanColle = {
 	remodel: {
 		categoryNameList: ['小口径主砲', '中口径主砲', '大口径主砲', '魚雷', '反潛裝備', '特種裝備'],
-		defaultChecked: ['20.3cm(2号)連装砲', '20.3cm(3号)連装砲', '41cm連装砲', '46cm三連装砲', '61cm五連装(酸素)魚雷', '九一式徹甲弾'],
-		equips: {
-			/* 小口径主砲,  */
-			'12.7cm連装砲': {
-				name: '12.7cm連装砲',
-				icon: '小口径主砲',
-				category: '小口径主砲',
-				detail: [{
-					assistant: '-',
-					enableDays: [0, 1, 2, 3, 4, 5, 6]
-				}],
-				remark: ''
-			},
-			'12.7cm連装砲B型改二': {
-				name: '12.7cm連装砲B型改二',
-				icon: '小口径主砲',
-				category: '小口径主砲',
-				detail: [{
-					assistant: '夕立改二, 绫波改二',
-					enableDays: [1, 2, 3]
-				}],
-				remark: ''
-			},
-			'10cm高角砲＋高射装置': {
-				name: '10cm高角砲＋高射装置',
-				icon: '高角砲',
-				category: '小口径主砲',
-				detail: [{
-					assistant: '秋月',
-					enableDays: [1, 2, 3, 4]
-				}],
-				remark: '消費装備:10cm連装高角砲'
-			},
-
-			/* 中口径主砲 */
-			'14cm単装砲': {
-				name: '14cm単装砲',
-				icon: '中口径主砲',
-				category: '中口径主砲',
-				detail: [{
-					assistant: '-',
-					enableDays: [0, 1, 2, 3, 4, 5, 6]
-				}],
-				remark: ''
-			},
-			'14cm連装砲': {
-				name: '14cm連装砲',
-				icon: '中口径主砲',
-				category: '中口径主砲',
-				detail: [{
-					assistant: '夕張',
-					enableDays: [1, 4]
-				}],
-				remark: ''
-			},
-			'15.5cm三連装砲': {
-				name: '15.5cm三連装砲',
-				icon: '中口径主砲',
-				category: '中口径主砲',
-				detail: [{
-					assistant: '最上',
-					enableDays: [5, 6]
-				}, {
-					assistant: '大淀',
-					enableDays: [0, 1]
-				}],
-				remark: ''
-			},
-			'20.3cm連装砲': {
-				name: '20.3cm連装砲',
-				icon: '中口径主砲',
-				category: '中口径主砲',
-				detail: [{
-					assistant: '青葉, 衣笠',
-					enableDays: [0, 1, 2, 3, 4, 5, 6]
-				}],
-				remark: ''
-			},
-			'20.3cm(2号)連装砲': {
-				name: '20.3cm(2号)連装砲',
-				icon: '中口径主砲',
-				category: '中口径主砲',
-				detail: [{
-					assistant: '妙高',
-					enableDays: [0, 1, 2]
-				}],
-				remark: ''
-			},
-			'20.3cm(3号)連装砲': {
-				name: '20.3cm(3号)連装砲',
-				icon: '中口径主砲',
-				category: '中口径主砲',
-				detail: [{
-					assistant: '三隈',
-					enableDays: [2, 3]
-				}],
-				remark: ''
-			},
-
-			/* 大口径主砲 */
-			'35.6cm連装砲': {
-				name: '35.6cm連装砲',
-				icon: '大口径主砲',
-				category: '大口径主砲',
-				detail: [{
-					assistant: '扶桑',
-					enableDays: [0, 5, 6]
-				}],
-				remark: ''
-			},
-			'38cm連装砲': {
-				name: '38cm連装砲',
-				icon: '大口径主砲',
-				category: '大口径主砲',
-				detail: [{
-					assistant: 'Bismarck',
-					enableDays: [4, 5, 6]
-				}],
-				remark: '初期の改修から消費装備が必要\n消費装備:35.6cm連装砲\n更新消費:41cm連装砲'
-			},
-			'38cm連装砲改': {
-				name: '38cm連装砲改',
-				icon: '大口径主砲',
-				category: '大口径主砲',
-				detail: [{
-					assistant: 'Bismarck',
-					enableDays: [0, 1, 2]
-				}],
-				remark: '初期の改修から消費装備が必要\n消費装備:41cm連装砲'
-			},
-			'41cm連装砲': {
-				name: '41cm連装砲',
-				icon: '大口径主砲',
-				category: '大口径主砲',
-				detail: [{
-					assistant: '長門',
-					enableDays: [2, 5, 6]
-				}, {
-					assistant: '陸奥',
-					enableDays: [0, 1, 4]
-				}],
-				remark: '初期の改修から消費装備が必要'
-			},
-			'試製46cm連装砲': {
-				name: '試製46cm連装砲',
-				icon: '大口径主砲',
-				category: '大口径主砲',
-				detail: [{
-					assistant: '大和',
-					enableDays: [0, 1]
-				}, {
-					assistant: '武蔵',
-					enableDays: [2, 3]
-				}],
-				remark: '初期の改修から消費装備が必要\n消費装備:41cm連装砲\n更新消費:41cm連装砲'
-			},
-			'46cm三連装砲': {
-				name: '46cm三連装砲',
-				icon: '大口径主砲',
-				category: '大口径主砲',
-				detail: [{
-					assistant: '大和',
-					enableDays: [5, 6]
-				}, {
-					assistant: '武蔵',
-					enableDays: [0, 1]
-				}],
-				remark: '初期の改修から消費装備が必要'
-			},
-
-			/* 魚雷 */
-			'61cm三連装魚雷': {
-				name: '61cm三連装魚雷',
-				icon: '魚雷',
-				category: '魚雷',
-				detail: [{
-					assistant: '吹雪',
-					enableDays: [4, 5, 6]
-				}, {
-					assistant: '叢雲',
-					enableDays: [0, 1, 2]
-				}],
-				remark: ''
-			},
-			'61cm三連装(酸素)魚雷': {
-				name: '61cm三連装(酸素)魚雷',
-				icon: '魚雷',
-				category: '魚雷',
-				detail: [{
-					assistant: '吹雪改二',
-					enableDays: [4, 5, 6]
-				}],
-				remark: '消費装備:61cm三連装魚雷\n更新装備:61cm四連装魚雷'
-			},
-			'61cm四連装魚雷': {
-				name: '61cm四連装魚雷',
-				icon: '魚雷',
-				category: '魚雷',
-				detail: [{
-					assistant: '-',
-					enableDays: [0, 1, 2, 5, 6]
-				}],
-				remark: ''
-			},
-			'61cm四連装(酸素)魚雷': {
-				name: '61cm四連装(酸素)魚雷',
-				icon: '魚雷',
-				category: '魚雷',
-				detail: [{
-					assistant: '北上, 大井',
-					enableDays: [0, 1, 2, 3, 4, 5, 6]
-				}],
-				remark: ''
-			},
-			'61cm五連装(酸素)魚雷': {
-				name: '61cm五連装(酸素)魚雷',
-				icon: '魚雷',
-				category: '魚雷',
-				detail: [{
-					assistant: '島風',
-					enableDays: [3, 4]
-				}],
-				remark: ''
-			},
-
-			/* 聲呐 */
-			'九四式爆雷投射機': {
-				name: '九四式爆雷投射機',
-				icon: '爆雷',
-				category: '反潛裝備',
-				detail: [{
-					assistant: '-',
-					enableDays: [3, 4]
-				}],
-				remark: ''
-			},
-			'三式爆雷投射機': {
-				name: '三式爆雷投射機',
-				icon: '爆雷',
-				category: '反潛裝備',
-				detail: [{
-					assistant: '-',
-					enableDays: [3, 4]
-				}],
-				remark: ''
-			},
-			'九三式水中聴音機': {
-				name: '九三式水中聴音機',
-				icon: '聲呐',
-				category: '反潛裝備',
-				detail: [{
-					assistant: '夕張',
-					enableDays: [0, 5, 6]
-				}, {
-					assistant: '五十鈴改二',
-					enableDays: [1, 5, 6]
-				}],
-				remark: ''
-			},
-			'三式水中探信儀': {
-				name: '三式水中探信儀',
-				icon: '聲呐',
-				category: '反潛裝備',
-				detail: [{
-					assistant: '夕張',
-					enableDays: [2, 3]
-				}, {
-					assistant: '五十鈴改二',
-					enableDays: [0, 2, 3]
-				}],
-				remark: ''
-			},
-
-			/* 特種裝備 */
-			'九一式徹甲弾': {
-				name: '九一式徹甲弾',
-				icon: '対艦強化弾',
-				category: '特種裝備',
-				detail: [{
-					assistant: '比叡',
-					enableDays: [3, 4, 5, 6]
-				}, {
-					assistant: '霧島',
-					enableDays: [0, 1, 5, 6]
-				}],
-				remark: ''
-			},
-			'一式徹甲弾': {
-				name: '一式徹甲弾',
-				icon: '対艦強化弾',
-				category: '特種裝備',
-				detail: [{
-					assistant: '金剛',
-					enableDays: [0, 5, 6]
-				}, {
-					assistant: '榛名',
-					enableDays: [1, 2, 3]
-				}],
-				remark: '初期の改修から消費装備が必要\n消費装備(初期～★5):九一式徹甲弾\n消費装備;(★6 ～★9):一式徹甲弾'
-			},
-			'91式高射装置': {
-				name: '91式高射装置',
-				icon: '高射装置',
-				category: '特種裝備',
-				detail: [{
-					assistant: '摩耶, 秋月',
-					enableDays: [0, 1, 5, 6]
-				}],
-				remark: '消費装備:12.7cm連装高角砲\n更新消費:10cm連装高角砲'
-			},
-			'94式高射装置': {
-				name: '94式高射装置',
-				icon: '高射装置',
-				category: '特種裝備',
-				detail: [{
-					assistant: '秋月',
-					enableDays: [0, 1, 2, 3, 4, 5, 6]
-				}, {
-					assistant: '吹雪改二',
-					enableDays: [0, 4, 5, 6]
-				}],
-				remark: '消費装備:10cm連装高角砲'
-			},
-			'25mm連装機銃': {
-				name: '25mm連装機銃',
-				icon: '対空機銃',
-				category: '特種裝備',
-				detail: [{
-					assistant: '五十鈴改二',
-					enableDays: [0, 5, 6]
-				}],
-				remark: ''
-			},
-			'25mm三連装機銃': {
-				name: '25mm三連装機銃',
-				icon: '対空機銃',
-				category: '特種裝備',
-				detail: [{
-					assistant: '五十鈴改二',
-					enableDays: [1, 2, 3]
-				}, {
-					assistant: '摩耶/摩耶改',
-					enableDays: [2, 3, 4]
-				}, {
-					assistant: '摩耶改二',
-					enableDays: [0, 1, 5, 6]
-				}],
-			},
-
-		},
+		defaultSelected: ['20.3cm(2号)連装砲', '20.3cm(3号)連装砲', '41cm連装砲', '46cm三連装砲', '61cm五連装(酸素)魚雷', '九一式徹甲弾'],
 
 		/*
 		 *  访问路径: kanColle.remodel.extractEquip(equipName)
@@ -425,7 +76,7 @@ var kanColle = {
 			for (i in names) {
 
 				var name = names[i];
-				if (name === this.defaultChecked[defaultIndex]) {
+				if (name === this.defaultSelected[defaultIndex]) {
 					defaultCheckedSts += 1;
 					defaultIndex++;
 				} else {
@@ -445,11 +96,11 @@ var kanColle = {
 				'対空機銃': 'anti-air-gun',
 				'高射装置': 'anti-air-fire-director',
 				'魚雷': 'torpedo',
-				'爆雷': 'anit-sub-weapon',
+				'爆雷': 'anti-sub-weapon',
 				'聲呐': 'soner',
-				'反潛裝備': 'anit-sub-weapon',
+				'反潛裝備': 'anti-sub-weapon',
 				'特種裝備': 'armour-piercing-shell',
-				'対艦強化弾' : 'armour-piercing-shell',
+				'対艦強化弾': 'armour-piercing-shell',
 			},
 
 			searchClass: function(category) {
@@ -478,4 +129,380 @@ var kanColle = {
 			}
 		}
 	}
+};
+
+kanColle.remodel.equips = {};
+
+/* 小口径主砲 */
+kanColle.remodel.equips['12.7cm連装砲'] = {
+	name: '12.7cm連装砲',
+	icon: '小口径主砲',
+	category: '小口径主砲',
+	detail: [{
+		assistant: '-',
+		enableDays: [0, 1, 2, 3, 4, 5, 6]
+	}],
+	remark: ''
+};
+
+kanColle.remodel.equips['12.7cm連装砲B型改二'] = {
+	name: '12.7cm連装砲B型改二',
+	icon: '小口径主砲',
+	category: '小口径主砲',
+	detail: [{
+		assistant: '夕立改二, 绫波改二',
+		enableDays: [1, 2, 3]
+	}],
+	remark: ''
+};
+
+kanColle.remodel.equips['10cm高角砲＋高射装置'] = {
+	name: '10cm高角砲＋高射装置',
+	icon: '高角砲',
+	category: '小口径主砲',
+	detail: [{
+		assistant: '秋月',
+		enableDays: [1, 2, 3, 4]
+	}],
+	remark: '消費装備:10cm連装高角砲'
+};
+
+/* 中口径主砲 */
+kanColle.remodel.equips['14cm単装砲'] = {
+	name: '14cm単装砲',
+	icon: '中口径主砲',
+	category: '中口径主砲',
+	detail: [{
+		assistant: '-',
+		enableDays: [0, 1, 2, 3, 4, 5, 6]
+	}],
+	remark: ''
+};
+
+kanColle.remodel.equips['14cm連装砲'] = {
+	name: '14cm連装砲',
+	icon: '中口径主砲',
+	category: '中口径主砲',
+	detail: [{
+		assistant: '夕張',
+		enableDays: [1, 4]
+	}],
+	remark: ''
+};
+
+kanColle.remodel.equips['15.5cm三連装砲'] = {
+	name: '15.5cm三連装砲',
+	icon: '中口径主砲',
+	category: '中口径主砲',
+	detail: [{
+		assistant: '最上',
+		enableDays: [5, 6]
+	}, {
+		assistant: '大淀',
+		enableDays: [0, 1]
+	}],
+	remark: ''
+};
+
+kanColle.remodel.equips['20.3cm連装砲'] = {
+	name: '20.3cm連装砲',
+	icon: '中口径主砲',
+	category: '中口径主砲',
+	detail: [{
+		assistant: '青葉, 衣笠',
+		enableDays: [0, 1, 2, 3, 4, 5, 6]
+	}],
+	remark: ''
+};
+
+kanColle.remodel.equips['20.3cm(2号)連装砲'] = {
+	name: '20.3cm(2号)連装砲',
+	icon: '中口径主砲',
+	category: '中口径主砲',
+	detail: [{
+		assistant: '妙高',
+		enableDays: [0, 1, 2]
+	}],
+	remark: ''
+};
+
+kanColle.remodel.equips['20.3cm(3号)連装砲'] = {
+	name: '20.3cm(3号)連装砲',
+	icon: '中口径主砲',
+	category: '中口径主砲',
+	detail: [{
+		assistant: '三隈',
+		enableDays: [2, 3]
+	}],
+	remark: ''
+};
+
+/* 大口径主砲 */
+kanColle.remodel.equips['35.6cm連装砲'] = {
+	name: '35.6cm連装砲',
+	icon: '大口径主砲',
+	category: '大口径主砲',
+	detail: [{
+		assistant: '扶桑',
+		enableDays: [0, 5, 6]
+	}],
+	remark: ''
+};
+
+kanColle.remodel.equips['38cm連装砲'] = {
+	name: '38cm連装砲',
+	icon: '大口径主砲',
+	category: '大口径主砲',
+	detail: [{
+		assistant: 'Bismarck',
+		enableDays: [4, 5, 6]
+	}],
+	remark: '初期の改修から消費装備が必要\n消費装備:35.6cm連装砲\n更新消費:41cm連装砲'
+};
+
+kanColle.remodel.equips['38cm連装砲改'] = {
+	name: '38cm連装砲改',
+	icon: '大口径主砲',
+	category: '大口径主砲',
+	detail: [{
+		assistant: 'Bismarck',
+		enableDays: [0, 1, 2]
+	}],
+	remark: '初期の改修から消費装備が必要\n消費装備:41cm連装砲'
+};
+
+kanColle.remodel.equips['41cm連装砲'] = {
+	name: '41cm連装砲',
+	icon: '大口径主砲',
+	category: '大口径主砲',
+	detail: [{
+		assistant: '長門',
+		enableDays: [2, 5, 6]
+	}, {
+		assistant: '陸奥',
+		enableDays: [0, 1, 4]
+	}],
+	remark: '初期の改修から消費装備が必要'
+};
+
+kanColle.remodel.equips['試製46cm連装砲'] = {
+	name: '試製46cm連装砲',
+	icon: '大口径主砲',
+	category: '大口径主砲',
+	detail: [{
+		assistant: '大和',
+		enableDays: [0, 1]
+	}, {
+		assistant: '武蔵',
+		enableDays: [2, 3]
+	}],
+	remark: '初期の改修から消費装備が必要\n消費装備:41cm連装砲\n更新消費:41cm連装砲'
+};
+
+kanColle.remodel.equips['46cm三連装砲'] = {
+	name: '46cm三連装砲',
+	icon: '大口径主砲',
+	category: '大口径主砲',
+	detail: [{
+		assistant: '大和',
+		enableDays: [5, 6]
+	}, {
+		assistant: '武蔵',
+		enableDays: [0, 1]
+	}],
+	remark: '初期の改修から消費装備が必要'
+};
+
+/* 魚雷 */
+kanColle.remodel.equips['61cm三連装魚雷'] = {
+	name: '61cm三連装魚雷',
+	icon: '魚雷',
+	category: '魚雷',
+	detail: [{
+		assistant: '吹雪',
+		enableDays: [4, 5, 6]
+	}, {
+		assistant: '叢雲',
+		enableDays: [0, 1, 2]
+	}],
+	remark: ''
+};
+
+kanColle.remodel.equips['61cm三連装(酸素)魚雷'] = {
+	name: '61cm三連装(酸素)魚雷',
+	icon: '魚雷',
+	category: '魚雷',
+	detail: [{
+		assistant: '吹雪改二',
+		enableDays: [4, 5, 6]
+	}],
+	remark: '消費装備:61cm三連装魚雷\n更新装備:61cm四連装魚雷'
+};
+
+kanColle.remodel.equips['61cm四連装魚雷'] = {
+	name: '61cm四連装魚雷',
+	icon: '魚雷',
+	category: '魚雷',
+	detail: [{
+		assistant: '-',
+		enableDays: [0, 1, 2, 5, 6]
+	}],
+	remark: ''
+};
+
+kanColle.remodel.equips['61cm四連装(酸素)魚雷'] = {
+	name: '61cm四連装(酸素)魚雷',
+	icon: '魚雷',
+	category: '魚雷',
+	detail: [{
+		assistant: '北上, 大井',
+		enableDays: [0, 1, 2, 3, 4, 5, 6]
+	}],
+	remark: ''
+};
+
+kanColle.remodel.equips['61cm五連装(酸素)魚雷'] = {
+	name: '61cm五連装(酸素)魚雷',
+	icon: '魚雷',
+	category: '魚雷',
+	detail: [{
+		assistant: '島風',
+		enableDays: [3, 4]
+	}],
+	remark: ''
+};
+
+/* 聲呐 */
+kanColle.remodel.equips['九四式爆雷投射機'] = {
+	name: '九四式爆雷投射機',
+	icon: '爆雷',
+	category: '反潛裝備',
+	detail: [{
+		assistant: '-',
+		enableDays: [3, 4]
+	}],
+	remark: ''
+};
+
+kanColle.remodel.equips['三式爆雷投射機'] = {
+	name: '三式爆雷投射機',
+	icon: '爆雷',
+	category: '反潛裝備',
+	detail: [{
+		assistant: '-',
+		enableDays: [3, 4]
+	}],
+	remark: ''
+};
+
+kanColle.remodel.equips['九三式水中聴音機'] = {
+	name: '九三式水中聴音機',
+	icon: '聲呐',
+	category: '反潛裝備',
+	detail: [{
+		assistant: '夕張',
+		enableDays: [0, 5, 6]
+	}, {
+		assistant: '五十鈴改二',
+		enableDays: [1, 5, 6]
+	}],
+	remark: ''
+};
+
+kanColle.remodel.equips['三式水中探信儀'] = {
+	name: '三式水中探信儀',
+	icon: '聲呐',
+	category: '反潛裝備',
+	detail: [{
+		assistant: '夕張',
+		enableDays: [2, 3]
+	}, {
+		assistant: '五十鈴改二',
+		enableDays: [0, 2, 3]
+	}],
+	remark: ''
+};
+
+/* 特種裝備 */
+kanColle.remodel.equips['九一式徹甲弾'] = {
+	name: '九一式徹甲弾',
+	icon: '対艦強化弾',
+	category: '特種裝備',
+	detail: [{
+		assistant: '比叡',
+		enableDays: [3, 4, 5, 6]
+	}, {
+		assistant: '霧島',
+		enableDays: [0, 1, 5, 6]
+	}],
+	remark: ''
+};
+
+kanColle.remodel.equips['一式徹甲弾'] = {
+	name: '一式徹甲弾',
+	icon: '対艦強化弾',
+	category: '特種裝備',
+	detail: [{
+		assistant: '金剛',
+		enableDays: [0, 5, 6]
+	}, {
+		assistant: '榛名',
+		enableDays: [1, 2, 3]
+	}],
+	remark: '初期の改修から消費装備が必要\n消費装備(初期～★5):九一式徹甲弾\n消費装備;(★6 ～★9):一式徹甲弾'
+};
+
+kanColle.remodel.equips['91式高射装置'] = {
+	name: '91式高射装置',
+	icon: '高射装置',
+	category: '特種裝備',
+	detail: [{
+		assistant: '摩耶, 秋月',
+		enableDays: [0, 1, 5, 6]
+	}],
+	remark: '消費装備:12.7cm連装高角砲\n更新消費:10cm連装高角砲'
+};
+
+kanColle.remodel.equips['94式高射装置'] = {
+	name: '94式高射装置',
+	icon: '高射装置',
+	category: '特種裝備',
+	detail: [{
+		assistant: '秋月',
+		enableDays: [0, 1, 2, 3, 4, 5, 6]
+	}, {
+		assistant: '吹雪改二',
+		enableDays: [0, 4, 5, 6]
+	}, {
+		assistant: '摩耶改二',
+		enableDays: [0, 4, 5, 6]
+	}],
+	remark: '消費装備:10cm連装高角砲'
+};
+
+kanColle.remodel.equips['25mm連装機銃'] = {
+	name: '25mm連装機銃',
+	icon: '対空機銃',
+	category: '特種裝備',
+	detail: [{
+		assistant: '五十鈴改二',
+		enableDays: [0, 5, 6]
+	}],
+	remark: ''
+};
+
+kanColle.remodel.equips['25mm三連装機銃'] = {
+	name: '25mm三連装機銃',
+	icon: '対空機銃',
+	category: '特種裝備',
+	detail: [{
+		assistant: '五十鈴改二',
+		enableDays: [1, 2, 3]
+	}, {
+		assistant: '摩耶/摩耶改',
+		enableDays: [2, 3, 4]
+	}, {
+		assistant: '摩耶改二',
+		enableDays: [0, 1, 5, 6]
+	}],
 };
